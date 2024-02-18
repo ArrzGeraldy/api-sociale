b71e807ba25514dfb4076f9d6305e42722ddc28b
ArrzGeraldy <Ucihas920@gmail.com>
2024-02-18 15:22:54 +0700

    commot1

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,19 @@
-import { Application, Router } from 'express'
-import { EventRouter } from './event.route'
-import { AuthRouter } from './auth.router'
-import { CategoryRouter } from './category.router'
-import { UserRouter } from './user.route'
+import { Application, Router } from "express";
+import { EventRouter } from "./event.route";
+import { AuthRouter } from "./auth.router";
+import { CategoryRouter } from "./category.router";
+import { UserRouter } from "./user.route";
 
 const _routes: Array<[string, Router]> = [
-  ['/event', EventRouter],
-  ['/auth', AuthRouter],
-  ['/category', CategoryRouter],
-  ['/user', UserRouter]
-]
+  ["/event", EventRouter],
+  ["/auth", AuthRouter],
+  ["/category", CategoryRouter],
+  ["/user", UserRouter],
+];
 
 export const routes = (app: Application) => {
-  _routes.forEach((route: [string, Router]) => {
-    const [url, router] = route
-    app.use(url, router)
-  })
-}
+  _routes.forEach((route) => {
+    const [url, router] = route;
+    app.use(url, router);
+  });
+};
