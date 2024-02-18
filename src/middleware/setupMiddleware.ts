dbf1c885dbd9b663a9e158c1164b56b00b6691ef
ArrzGeraldy <Ucihas920@gmail.com>
2024-02-18 14:45:36 +0700

    cors

--- src/middleware/setupMiddleware.ts
+++ src/middleware/setupMiddleware.ts
@@ -1,17 +1,17 @@
-import { Application } from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import { Application } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const setupMiddleware = (app: Application) => {
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(bodyParser.json())
-  app.use(cors())
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
   app.use((req, res, next) => {
-    res.setHeader('Acces-Control-Allow-Origin', '*')
-    res.setHeader('Acces-Control-Allow-Methods', '*')
-    res.setHeader('Acces-Control-Allow-Headers', '*')
-    next()
-  })
-}
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
+};
 
-export default setupMiddleware
+export default setupMiddleware;
