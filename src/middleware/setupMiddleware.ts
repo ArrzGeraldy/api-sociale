fe3d7ac661df02bcfb556dbf35382f824c23160a
ArrzGeraldy <Ucihas920@gmail.com>
2024-02-21 17:45:03 +0700

    cors perbaikan

--- src/middleware/setupMiddleware.ts
+++ src/middleware/setupMiddleware.ts
@@ -5,11 +5,12 @@ import bodyParser from "body-parser";
 const setupMiddleware = (app: Application) => {
   app.use(cors());
   app.use((req, res, next) => {
-    res.setHeader("Acces-Control-Allow-Origin", "*");
-    res.setHeader("Acces-Control-Allow-Methods", "*");
-    res.setHeader("Acces-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
     next();
   });
+
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 };
